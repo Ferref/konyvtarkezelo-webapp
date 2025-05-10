4f9085c4c58edc32649f793edcfbdf611a1081e9
Ferenc Kovács <kovacsferenc026@gmail.com>
2025-05-10 19:36:55 +0200

    Confirm gomb hozzáadva

---
+++ laravel/public/js/handler.js
@@ -0,0 +1,10 @@
+const confirmButtons = document.getElementsByClassName('confirmed');
+
+Array.from(confirmButtons).forEach(button => {
+    button.addEventListener('click', event => {
+        const ok = window.confirm('Are you sure?');
+        if (!ok) {
+            event.preventDefault();
+        }
+    });
+});
